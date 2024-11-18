2c0c08a51e93d9ebed57daf286fdca6259211706
jkim <jkim@blast-team.com>
2024-11-18 12:05:36 +0900

    feat: decrease, reset test add

--- counter-testing/src/components/Counter.tsx
+++ counter-testing/src/components/Counter.tsx
@@ -1,18 +1,34 @@
 "use client";
 
-import React, {Fragment, useState} from "react";
+import React, { Fragment, useState } from "react";
 
 interface CounterProps {
   initValue?: number;
 }
 
-const MyCounter = ({initValue = 0}: CounterProps) => {
+const MyCounter = ({ initValue = 0 }: CounterProps) => {
   const [count, setCount] = useState(initValue);
   return (
     <Fragment>
       <div role="count">{count}</div>
-      <button onClick={() => setCount((prevState) => prevState + 1)}>+</button>
-      <button onClick={() => setCount((prevState) => prevState - 1)}>-</button>
+      <button
+        className="w-4 h-4 border-white border grid place-content-center"
+        onClick={() => setCount((prevState) => prevState + 1)}
+      >
+        +
+      </button>
+      <button
+        className="w-4 h-4 border-white border grid place-content-center"
+        onClick={() => setCount((prevState) => prevState - 1)}
+      >
+        -
+      </button>
+      <button
+        className="w-10 h-4 border-white border grid place-content-center"
+        onClick={() => setCount(initValue)}
+      >
+        reset
+      </button>
     </Fragment>
   );
 };
